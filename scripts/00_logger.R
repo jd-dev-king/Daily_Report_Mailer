a5cc95884df4cbf8f6e80c3a91db35b321e9c4d6
dev-king-jd <jeremiah.lupton@cognizant.com>
2026-07-16 23:25:37 -0400

    Fix logging folder creation for CI pipeline

--- scripts/00_logger.R
+++ scripts/00_logger.R
@@ -4,10 +4,17 @@
 
 log_message <- function(message){
 
+  # Create logs folder if missing
+  if(!dir.exists("logs")){
+    dir.create("logs")
+  }
+
+
   timestamp <- format(
     Sys.time(),
     "%Y-%m-%d %H:%M:%S"
   )
+
 
   log_entry <- paste(
     timestamp,
@@ -15,11 +22,14 @@ log_message <- function(message){
     message
   )
 
+
   write(
     log_entry,
     file = "logs/automation_log.txt",
     append = TRUE
   )
 
+
   cat(log_entry, "\n")
+
 }
